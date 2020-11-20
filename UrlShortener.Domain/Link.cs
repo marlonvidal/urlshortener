bca8a66bfa70854d86f008f3de4813241f4202c9
Marlon Vidal <marlonvidalferreira@hotmail.com>
2020-11-19 21:38:34 -0300

    Removing domain files

---
+++ UrlShortener.Domain/Link.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UrlShortener.Domain
+{
+    public class Link
+    {
+        public string URL { get; set; }
+        
+        public string ComputeShortenedUrl()
+        {
+
+        }
+    }
+}
