5458761bd0ad8d32da1d63da12d87992fa55332e
Marlon Vidal <marlonvidalferreira@hotmail.com>
2020-11-19 21:30:02 -0300

    Implemented unit tests and logic for url shortening process

--- UrlShortener.Domain/Link.cs
+++ UrlShortener.Domain/Link.cs
@@ -16,7 +16,22 @@ namespace UrlShortener.Domain
         public string ComputeShortenedUrl()
         {
             var hashed = Hash.GetSha256Hash(URL);
-            return Convert.ToBase64String(hashed);
+            
+            if(hashed != null)
+            {
+                var base64 = Convert.ToBase64String(hashed);
+
+                /*
+                 * Accordingly to https://www.educative.io/courses/grokking-the-system-design-interview/m2ygV4E81AR article
+                 * Using base64 encoding for SHA256 hashing mechanism strings, 
+                 * a 6 letters long key would result in 64 ^ 6 = ~68.7 billion possible strings.
+                 * I believe this will be enought for our requirements
+                */
+
+                return base64.Substring(0, 6);
+            }
+
+            return string.Empty;
         }
     }
 }
---
+++ UrlShortener.Tests/Domain/LinkTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UrlShortener.Common;
+using UrlShortener.Domain;
+using Xunit;
+
+namespace UrlShortener.Tests.Domain
+{
+    public class LinkTests
+    {
+        [Fact]
+        public void Should_Compute_shortented_url()
+        {
+            string url = "https://google.com/test";
+
+            var link = new Link();
+            link.URL = url;
+
+            var hashedUrl = Hash.GetSha256Hash(url);
+            var base64 = Convert.ToBase64String(hashedUrl);
+            var expected = base64.Substring(0, 6);
+
+            var result = link.ComputeShortenedUrl();
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Empty_url_should_compute_nothing()
+        {
+            var link = new Link();
+            var expected = string.Empty;
+
+            var result = link.ComputeShortenedUrl();
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
